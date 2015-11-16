473070ca2c82226e329a9c8d26519732a4993766
Łukasz Oleś <loles@mirantis.com>
2015-11-16 14:01:38 +0100

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'result in subunit format.'
     ),
     long_description="",
-    version='0.0.1',
+    version='0.0.2',
     license='Apache 2',
     author='Łukasz Oleś',
     author_email='loles@mirantis.com',
